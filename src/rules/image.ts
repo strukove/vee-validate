8e0ba59a185f4315ec32b6be3856db8c9375c950
Eugene Strukov <eugene.strukov@creuna.com>
2020-02-19 09:47:23 +0200

    fix: remove needless check

--- src/rules/image.ts
+++ src/rules/image.ts
@@ -1,11 +1,5 @@
-import { isNullOrUndefined } from '../utils';
-
 const validate = (files: File | File[]) => {
   const regex = /\.(jpg|svg|jpeg|png|bmp|gif)$/i;
-
-  if (isNullOrUndefined(files)) {
-    return false;
-  }
 
   if (Array.isArray(files)) {
     return files.every(file => regex.test(file.name));
